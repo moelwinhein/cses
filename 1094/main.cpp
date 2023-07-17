d41cca1ee2ecc775fec1501374a10587a2fbcf74
Moe Lwin Hein <hein.moelwin@gmail.com>
2023-07-17 15:37:13 +0800

    Update tab with spaces

--- 1094/main.cpp
+++ 1094/main.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int main() {
-	int n; 
-	long prev = -1, curr = -1, count = 0;
-	scanf("%d", &n);
-	scanf("%ld", &prev);
-	n--;
-	while (n--) {
-		scanf("%ld", &curr);
-		if (curr < prev) {
-			long diff = prev - curr;
-			count += diff;
-			curr += diff;
-		}
-		prev = curr;
-	}
-	cout << count << endl;
-	return 0;
+    int n;
+    long prev = -1, curr = -1, count = 0;
+    scanf("%d", &n);
+    scanf("%ld", &prev);
+    n--;
+    while (n--) {
+        scanf("%ld", &curr);
+        if (curr < prev) {
+            long diff = prev - curr;
+            count += diff;
+            curr += diff;
+        }
+        prev = curr;
+    }
+    cout << count << endl;
+    return 0;
 }
